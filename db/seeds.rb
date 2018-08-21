1f0ad035bbdd9ff6eb4bf2f290b816013e7932b1
Heather Lee <heather.k.lee@gmail.com>
2018-08-20 20:11:13 -0400

    update seeds.rb with sample user information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create User
+
+User.create(first_name: "Adel", last_name: "Koljenovic", email: "adel@actualize.com", phone_number: "1234567890", short_bio: "super cool dude", linkedin_url: "www.linkedin.com/adel", twitter_handle: "@adel", website_url: "www.adel.com", online_resume_url: "www.adelresume.com", github_url: "www.github.com/adelkoljenovic", photo_url: "www.adelphoto.com")
+User.create(first_name: "adam", last_name: "Koljenovic", email: "adam@actualize.com", phone_number: "1234567890", short_bio: "super cool dude", linkedin_url: "www.linkedin.com/adam", twitter_handle: "@adam", website_url: "www.adam.com", online_resume_url: "www.adamresume.com", github_url: "www.github.com/adamkoljenovic", photo_url: "www.adamphoto.com")
+User.create(first_name: "Abe", last_name: "Koljenovic", email: "Abe@actualize.com", phone_number: "1234567890", short_bio: "super cool dude", linkedin_url: "www.linkedin.com/Abe", twitter_handle: "@Abe", website_url: "www.Abe.com", online_resume_url: "www.Aberesume.com", github_url: "www.github.com/Abekoljenovic", photo_url: "www.Abephoto.com")
