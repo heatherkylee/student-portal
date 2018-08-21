c20f07cedd7bb31c672ba312d0df4f77c2e286bc
Bongardino <bongardino@gmail.com>
2018-08-20 21:00:43 -0400

    add edit-user-page vue model / html template add bootstrap

--- public/index.js
+++ public/index.js
@@ -1,10 +1,40 @@
 /* global Vue, VueRouter, axios */
 
+
+var UserEditPage = {
+  template: "#user-edit-page",
+  data: function() {
+    return {
+      message: "UserEditPage",
+      userInfo: {
+        first_name: "first_name",
+        last_name: "last_name",
+        email: "email",
+        phone_number: "phone_number",
+        short_bio: "short_bio",
+        linkedin_url: "linkedin_url",
+        twitter_handle: "twitter_handle",
+        website_url: "website_url",
+        online_resume_url: "online_resume_url",
+        github_url: "github_url",
+        photo_url: "photo_url"
+      }
+    };
+  },
+  created: function() {
+    // axios.get('/api/user').then(function (response) {
+    //   this.user = response.data;
+    // }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
 var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Home"
     };
   },
   created: function() {},
@@ -13,7 +43,10 @@ var HomePage = {
 };
 
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+  { path: "/", component: HomePage },
+  { path: "/edit", component: UserEditPage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
